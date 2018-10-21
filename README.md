2743f2e52648d3d96460fb2aec0a22a2a5d20ce5
Chris Suran <suran.c@gmail.com>
2018-10-21 03:03:38 -0400

    Adding initial scripts

---
+++ README.md
@@ -0,0 +1,10 @@
+# Mangler Club NGINX Deployment
+
+This repository contains the code to deploy the Mangler Club sites on a VM.  
+
+## Dependancies
+* Docker
+* Systemd
+
+## To Run:
+To start the site, run `./setup.sh`
---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Install nginx, copy conifg, and run
+sudo apt install nginx
+cp containers.conf /etc/nginx/conf.d/
+sudo systemctl enable nginx
+sudo systemctl start nginx
+
+# Start containers
+docker run -d -p 8080:80 suranc/images:0.6
+docker run -d -p 8081:80 suranc/ytlnd:0.1
+docker run -d -p 8082:80 suranc/ytrnd:0.1
